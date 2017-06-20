dc533041c1bffea1063682069b73f7f7f3de7c99
Asiantech <asiantech@Tam-Tinh-Te.local>
2017-06-20 17:58:31 +0700

    init 4 fragment

---
+++ app/src/main/java/com/example/asiantech/myprojectkotlin/fragments/BellFollowMyPostFragment.kt
@@ -0,0 +1,12 @@
+package com.example.asiantech.myprojectkotlin.fragments
+
+import android.support.v4.app.Fragment
+
+/**
+ * BellFollowMyPostFragment
+ *
+ * @author at-ToanNguyen
+ */
+class BellFollowMyPostFragment:Fragment(){
+
+}
---
+++ app/src/main/java/com/example/asiantech/myprojectkotlin/fragments/CollectMyNewsFragment.kt
@@ -0,0 +1,12 @@
+package com.example.asiantech.myprojectkotlin.fragments
+
+import android.support.v4.app.Fragment
+
+/**
+ * CollectMyNewsFragment
+ *
+ * @author at-ToanNguyen
+ */
+class CollectMyNewsFragment : Fragment() {
+
+}
